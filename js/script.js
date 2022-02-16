051bf42fe575a7968a385308c748c60c7d4711cb
hasibmiraz <hasibmiraz09@gmail.com>
2022-02-17 00:13:08 +0600

    CSS Modified

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const modal = document.getElementById("warning-modal");
+  modal.style.display = "flex";
+});
+document.getElementById("hide-modal").addEventListener("click", function () {
+  const modal = document.getElementById("warning-modal");
+  modal.style.display = "none";
+});

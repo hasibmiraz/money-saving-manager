83e32e97b4696e68d1b5308d7277992281f5ea50
hasibmiraz <hasibmiraz09@gmail.com>
2022-02-16 22:41:48 +0600

    UI Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     extend: {
       colors: {
         "title-color": "#31A5AC",
+        "main-bg": "#E5E5E5",
+        "form-bg": "#F8F8F8",
+        "form-color": "#59A5FF",
       },
     },
   },

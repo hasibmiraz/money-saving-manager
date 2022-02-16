1a551e90d8522e5d00630c25b6817ca298ae81ac
hasibmiraz <hasibmiraz09@gmail.com>
2022-02-17 00:58:52 +0600

    Income Expense Calculation Complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
+// Get input fields
+const income = document.getElementById("income-amount");
+const food = document.getElementById("food-amount");
+const rent = document.getElementById("rent-amount");
+const cloth = document.getElementById("cloth-amount");
+
+// Input value function
+function getValue(field) {
+  if (field.value === "") {
+    return 0;
+  } else {
+    return field.value;
+  }
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const modal = document.getElementById("warning-modal");
-  modal.style.display = "flex";
-});
-document.getElementById("hide-modal").addEventListener("click", function () {
-  const modal = document.getElementById("warning-modal");
-  modal.style.display = "none";
+  const totalIncome = parseInt(getValue(income));
+
+  const totalExpense =
+    parseInt(getValue(food)) +
+    parseInt(getValue(rent)) +
+    parseInt(getValue(cloth));
+
+  const totalBalance = totalIncome - totalExpense;
+
+  document.getElementById("total-expenses").innerText = totalExpense;
+  document.getElementById("total-balance").innerText = totalBalance;
 });
